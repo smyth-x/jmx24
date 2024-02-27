2c6c2eaa49a904d5ace599b5471da24c41382ae0
smyth-x <161477651+smyth-x@users.noreply.github.com>
2024-02-27 21:47:52 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,36 +1,65 @@
-const CACHE_NAME = `temperature-converter-v1`;
+// The version of the cache.
+const VERSION = "v1";
 
-// Use the install event to pre-cache all initial resources.
-self.addEventListener('install', event => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    cache.addAll([
-      '/',
-      '/converter.js',
-      '/converter.css'
-    ]);
-  })());
+// The name of the cache
+const CACHE_NAME = `period-tracker-${VERSION}`;
+
+// The static resources that the app needs to function.
+const APP_STATIC_RESOURCES = [
+  "/",
+  "/index.html",
+  "/app.js",
+  "/style.css",
+  "/icons/wheel.svg",
+];
+
+// On install, cache the static resources
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(APP_STATIC_RESOURCES);
+    })(),
+  );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith((async () => {
-    const cache = await caches.open(CACHE_NAME);
+// delete old caches on activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const names = await caches.keys();
+      await Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        }),
+      );
+      await clients.claim();
+    })(),
+  );
+});
 
-    // Get the resource from the cache.
-    const cachedResponse = await cache.match(event.request);
-    if (cachedResponse) {
-      return cachedResponse;
-    } else {
-        try {
-          // If the resource was not in the cache, try the network.
-          const fetchResponse = await fetch(event.request);
+// On fetch, intercept server requests
+// and respond with cached responses instead of going to network
+self.addEventListener("fetch", (event) => {
+  // As a single page app, direct app to always go to cached home page.
+  if (event.request.mode === "navigate") {
+    event.respondWith(caches.match("/"));
+    return;
+  }
 
-          // Save the resource in the cache and return it.
-          cache.put(event.request, fetchResponse.clone());
-          return fetchResponse;
-        } catch (e) {
-          // The network failed.
-        }
-    }
-  })());
+  // For all other requests, go to the cache first, and then the network.
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      const cachedResponse = await cache.match(event.request.url);
+      if (cachedResponse) {
+        // Return the cached response if it's available.
+        return cachedResponse;
+      }
+      // If resource isn't in the cache, return a 404.
+      return new Response(null, { status: 404 });
+    })(),
+  );
 });
